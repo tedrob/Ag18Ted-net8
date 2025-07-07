f066791a559da090422aca61c3fb592c5cdb7634
tedrobinson <tfqbtd@aol.com>
2025-07-07 02:55:56 -0400

    update nav 7-6

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,31 +1,41 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { HasRoleDirective } from '../_directives/has-role.directive';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule, BsDropdownModule, RouterLink, RouterLinkActive, HasRoleDirective],
+  imports: [
+    CommonModule,
+    FormsModule,
+    BsDropdownModule,
+    RouterLink,
+    RouterLinkActive,
+    HasRoleDirective,
+  ],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.css'
+  styleUrl: './nav.component.css',
 })
-export class NavComponent {
-  accountService = inject(AccountService)
-  private router = inject(Router)
-  private toastr = inject(ToastrService)
-  model: any = {}
+export class NavComponent implements OnInit {
+  public accountService = inject(AccountService);
+  private router = inject(Router);
+  private toastr = inject(ToastrService);
+  model: any = {};
+
+  ngOnInit(): void {}
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: _ => {
-        this.router.navigateByUrl('/members')
+      next: (_) => {
+        this.router.navigateByUrl('/members');
+        this.model = {};
       },
-      error: error => this.toastr.error(error.error)
-    })
+    });
   }
 
   logout() {
